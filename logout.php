7bf0906b95836ec136b50564970ea4b5a484fe44
qnay31 <rizkyamien@gmail.com>
2022-01-31 10:40:18 +0700

    updateV2

--- logout.php
+++ logout.php
@@ -7,14 +7,7 @@ require 'function.php';
 $date   = date("Y-m-d H:i:s");
 $ip     = get_client_ip();
 
-$q = mysqli_query($conn, "SELECT akun_pengurus.nama, list_divisi.posisi
-
-FROM akun_pengurus
-JOIN list_divisi ON list_divisi.id_pengurus = akun_pengurus.id_pengurus WHERE akun_pengurus.username = '$_SESSION[username]' ");
-
-$data   = mysqli_fetch_array($q);
-
-$query = mysqli_query($conn, "INSERT INTO 2022_log_aktivity VALUES('', '$_SESSION[nama]', '$data[posisi]', '$ip', '$date', '$_SESSION[nama] $data[posisi] Telah Logout')");
+$query = mysqli_query($conn, "INSERT INTO 2022_log_aktivity VALUES('', '$_SESSION[nama]', '$_SESSION[posisi]', '$ip', '$date', '$_SESSION[nama] $_SESSION[posisi] Telah Logout dari halamannya')");
 // die(var_dump($query));
 session_start();
 $SESSION = [];
